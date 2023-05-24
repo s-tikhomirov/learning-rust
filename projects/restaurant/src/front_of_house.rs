d5bd99c6f512d0a3e06344df038770f75b3a3261
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2023-05-24 17:12:48 +0200

    add notes on Chapter 7

---
+++ projects/restaurant/src/front_of_house.rs
@@ -0,0 +1,9 @@
+pub mod hosting;
+
+mod front_of_house {
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+    }
+}
---
+++ projects/restaurant/src/lib.rs
@@ -0,0 +1,33 @@
+mod front_of_house;
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
+
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
